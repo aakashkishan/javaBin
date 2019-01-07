6db161732e881dc09a2c36ec182f8973366aecf3
aakash kishan <aakash_kishan@outlook.com>
2019-01-07 03:19:12 -0800

    Substring Replacement in String

---
+++ Arrays/replaceCharacter.java
@@ -0,0 +1,52 @@
+import java.lang.*;
+
+public class replaceCharacter {
+
+    public static void main(String args[]) {
+
+        String str = "Hello! Good Morning! Have a good day!";
+        String targetStr = " ";
+        System.out.println("Original String: " + str);
+        System.out.println("Modified String: " + replaceChar(str, targetStr));
+
+    }
+
+    public static String replaceChar(String str, String targetStr) {
+
+        int len = str.length();
+        String replaceStr = "%20";
+        String[] strArr = str.split(targetStr);
+        String result = "";
+
+        int counter = 0;
+        for(int i = 0; i < strArr.length; i++) {
+            if(i != strArr.length - 1) {
+                result += strArr[i] + replaceStr;
+            } else {
+                result += strArr[i];
+            }
+        }
+
+        return result;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
