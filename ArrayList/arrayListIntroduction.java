5be18d090d4bb566cd1b27a95d7259e99346a51c
aakash kishan <aakash_kishan@outlook.com>
2019-01-10 02:39:52 -0800

    ArrayList Introduction Phase1

---
+++ ArrayList/arrayListIntroduction.java
@@ -0,0 +1,69 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class arrayListIntroduction {
+
+    public static void main(String args[]) throws IOException {
+
+        int n = 5;
+
+        ArrayList<Integer> arrList = new ArrayList<Integer>(n);
+        for(int i = 1; i <= n; i++) {
+            arrList.add(i);
+        }
+
+        System.out.println(arrList);
+        // Argument passed to remove is the Index of the ArrayList
+        arrList.remove(4);
+        System.out.println(arrList);
+
+        System.out.println("Elements of ArrayList: ");
+        for(int i = 0; i < arrList.size(); i++) {
+            System.out.println(arrList.get(i) + " ");
+        }
+
+        // ********************* ARRAYLIST METHODS *************************
+        ArrayList<String> studentList = new ArrayList<String>(n);
+        String[] strArr = {"Ram", "Gopal", "Verma", "Rohit"};
+
+        for(String str : strArr) {
+            studentList.add(str);
+        }
+
+        System.out.println("Student Names: ");
+        // ArrayList forEach() function
+        studentList.forEach((x) -> printMessage(x));
+
+    }
+
+    public static void printMessage(String x) {
+
+        System.out.println(x);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
