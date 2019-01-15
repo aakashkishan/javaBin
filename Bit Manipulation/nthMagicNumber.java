7f00e6b46c0bd955ad19725344ddf95ad5854b00
aakash kishan <aakash_kishan@outlook.com>
2019-01-15 03:27:55 -0500

    Nth Magic Number - Bit Manipulation

---
+++ Bit Manipulation/nthMagicNumber.java
@@ -0,0 +1,61 @@
+/* Objective - given a number n, generate the nth magic number
+   NOTE : A Magic Number is a power of 5 or sum of unique powers of 5 
+   STRATEGY : The magic number is a perfect power of 5 when n is a power of 2 
+   example : 5, 25, 30, 125, 130, 150, 155, ...  */
+
+import java.lang.*;
+import java.io.*;
+
+public class nthMagicNumber {
+
+    public static void main(String args[]) {
+
+        int n = 6;
+        System.out.println("Computed Magic Number: " + computeMagicNumber(n));
+
+    }
+
+    public static int computeMagicNumber(int n) {
+
+        int result = 0;
+        int exponenet = 1;
+
+        // Iterate through the bits
+        while(n != 0) {
+
+            exponenet = exponenet * 5;
+
+            // If last bit of n is set to 1
+            if((int) (n & 1) == 1) {
+                result += exponenet;
+            }
+
+            // Divide the number n by 2
+            n >>= 1;
+
+        }
+
+        return result;
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
