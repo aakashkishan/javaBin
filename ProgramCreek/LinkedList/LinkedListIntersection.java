d4abb60a6c0cc54011eeefb913f3f760ef5ef18e
aakash kishan <aakash_kishan@outlook.com>
2019-07-02 02:25:49 -0400

    Alternate Approach for finding the Intersection of Two Linked Lists

--- ProgramCreek/LinkedList/LinkedListIntersection.java
+++ ProgramCreek/LinkedList/LinkedListIntersection.java
@@ -28,6 +28,55 @@ class Solution {
         return null;
         
     }
+
+    public ListNode altGetIntersectionNode(ListNode headA, ListNode headB) {
+
+        if(headA == null || headB == null) {
+            return null;
+        }
+
+        int len1 = 0;
+        int len2 = 0;
+        ListNode ptr1 = headA;
+        ListNode ptr2 = headB;
+    
+        while(ptr1 != null) {
+            len1 += 1;
+            ptr1 = ptr1.next;
+        }
+
+        while(ptr2 != null) {
+            len2 += 1;
+            ptr2 = ptr2.next;
+        }
+
+        if(len1 > len2) {
+            int difference = len1 - len2;
+            int i = 0;
+            while(i < difference) {
+                headA = headA.next;
+                i += 1;
+            }
+        }
+        else {
+            int difference = len2 - len1;
+            int i = 0;
+            while(i < difference) {
+                headB = headB.next;
+                i += 1;
+            }
+        }
+
+        while(headA != null && headB != null) {
+            if(headA.val == headB.val) {
+                return headA;
+            }
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
+
+    }
 }
 
 public class LinkedListIntersection {
@@ -44,7 +93,7 @@ public class LinkedListIntersection {
 
         ListNode headB = new ListNode(3);
         headB.next = intersection;
-        ListNode intersection_node = new Solution().getIntersectionNode(headA, headB);
+        ListNode intersection_node = new Solution().altGetIntersectionNode(headA, headB);
         System.out.println("The Intersection Node of the Two Linked Lists: " + intersection_node.val);
 
     }
