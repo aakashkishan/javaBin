dfcdd71635de3e2983bff6dd280538a92c662064
aakash kishan <aakash_kishan@outlook.com>
2019-09-09 01:15:05 -0700

    Reverse Zig Zag Traversal of Binary Tree

---
+++ ProgramCreek/Interview Questions/ReverseZigZagTraversal.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { 
+        val = x; 
+    }
+}
+
+public class ReverseZigZagTraversal {
+
+    public static List<Integer> getZigZag(TreeNode root) {
+
+        List<Integer> list = new ArrayList<Integer>();
+        LinkedList<TreeNode> nodes = new LinkedList<>();
+        LinkedList<Integer> ll = new LinkedList<>();
+        Stack<Integer> stack = new Stack<>();
+        if(root == null) {
+            return list;
+        }
+
+        nodes.add(root);
+        nodes.add(null);
+        while(!nodes.isEmpty()) {
+            TreeNode curr_node = nodes.poll();
+            if(curr_node == null) {
+                if(!nodes.isEmpty()) {
+                    nodes.add(null);
+                }
+                while(!ll.isEmpty()) {
+                    stack.push(ll.poll());
+                }
+            }
+            else {
+                ll.offer(curr_node.val);
+                if(curr_node.left != null) {
+                    nodes.offer(curr_node.left);
+                }
+                if(curr_node.right != null) {
+                    nodes.offer(curr_node.right);
+                }
+            }
+        }
+
+        while(!stack.isEmpty()) {
+            list.add(stack.pop());
+        }
+        return list;
+
+    }
+
+    public static void main(String args[]) {
+
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(7);
+
+        List<Integer> result_list = getZigZag(root);
+        ListIterator itr = result_list.listIterator();
+        System.out.println("The Zig Zag Tree Traversal: ");
+        while(itr.hasNext()) {
+            System.out.print(itr.next() + " ");
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
